756553f94ca9663add4c835743e7c7f9ca612270
Talha Jafar <talhajafar37@outlook.com>
2021-04-20 21:52:11 -0700

    brand update

--- src/components/general/Navbar/navbar.js
+++ src/components/general/Navbar/navbar.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 const Main_Nav = () => {
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand nav-link" to="/">PeaceCoin</a>
+            <Link className="navbar-brand nav-item" to="/">PeaceCoin</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
